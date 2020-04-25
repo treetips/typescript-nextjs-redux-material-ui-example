13b1ef3dff3040ec3d69e8efaab7640cde4459df
username <username@users.noreply.github.com>
2020-04-26 04:39:27 +0900

    refactor: changing the page on the client side

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
 import { Typography } from "@material-ui/core"
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
-import React from "react"
+import React, { useEffect } from "react"
 import { SpacingPaper } from "../components/atoms"
 import { HeaderArticleContainer } from "../components/organisms"
 import { Layout } from "../components/templates"
+import { Page } from "../constants"
+import { usePage } from "../hooks"
 
 const useStyles = makeStyles((_: Theme) =>
   createStyles({
@@ -15,6 +17,12 @@ type Props = {}
 
 function NotFoundError(props: Props) {
   const classes = useStyles(props)
+  const { changePage } = usePage()
+
+  useEffect(() => {
+    changePage(Page.ERROR)
+  }, [])
+
   return (
     <Layout className={classes.root}>
       <HeaderArticleContainer>
